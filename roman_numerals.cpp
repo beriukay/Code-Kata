64fabe1dade3d98e1c62b2bbd5183236c651b602
paul <beriukay@gmail.com>
2014-03-02 17:04:40 -0900

    Fail first test

--- roman_numerals.cpp
+++ roman_numerals.cpp
@@ -1,12 +1,16 @@
 /* Paul Gentemann
  * File Name : roman_numerals.cpp
  * Creation Date : 02-23-2014
- * Last Modified : Sun 02 Mar 2014 04:56:47 PM AKST
+ * Last Modified : Sun 02 Mar 2014 05:02:34 PM AKST
  */
 
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
+char numeral(int a)
+{   
+    return ' '; 
+}
 
 TEST_CASE( "Roman numeral computation", "[numeral]" ) 
 {
