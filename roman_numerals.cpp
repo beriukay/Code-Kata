204d347684ef09171bfac4b0a2610f3165f452eb
paul <beriukay@gmail.com>
2014-03-03 00:02:41 -0900

    add test for 7, which incidentally passes already

--- roman_numerals.cpp
+++ roman_numerals.cpp
@@ -1,7 +1,7 @@
 /* Paul Gentemann
  * File Name : roman_numerals.cpp
  * Creation Date : 02-23-2014
- * Last Modified : Mon 03 Mar 2014 12:01:28 AM AKST
+ * Last Modified : Mon 03 Mar 2014 12:02:18 AM AKST
  */
 
 #define CATCH_CONFIG_MAIN
@@ -32,4 +32,5 @@ TEST_CASE( "Roman numeral computation", "[numeral]" )
     REQUIRE( numeral(3) == "III" );
     REQUIRE( numeral(5) == "V" );
     REQUIRE( numeral(6) == "VI" );
+    REQUIRE( numeral(7) == "VII" );
 }
