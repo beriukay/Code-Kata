e873d8f3f3d790fafe8576d03a20cd15fa967638
paul <beriukay@gmail.com>
2014-03-02 19:33:28 -0900

    second test case

--- roman_numerals.cpp
+++ roman_numerals.cpp
@@ -1,7 +1,7 @@
 /* Paul Gentemann
  * File Name : roman_numerals.cpp
  * Creation Date : 02-23-2014
- * Last Modified : Sun 02 Mar 2014 07:21:47 PM AKST
+ * Last Modified : Sun 02 Mar 2014 07:33:17 PM AKST
  */
 
 #define CATCH_CONFIG_MAIN
@@ -15,4 +15,5 @@ char numeral(int a)
 TEST_CASE( "Roman numeral computation", "[numeral]" ) 
 {
     REQUIRE( numeral(1) == '1' );
+    REQUIRE( numeral(2) == '2' );
 }
