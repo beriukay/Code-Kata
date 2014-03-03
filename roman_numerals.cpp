515d186bc72a77dc3793c4e6049dbe616654b62b
paul <beriukay@gmail.com>
2014-03-02 22:29:30 -0900

    Second test fails because changing from char to char array

--- roman_numerals.cpp
+++ roman_numerals.cpp
@@ -1,7 +1,7 @@
 /* Paul Gentemann
  * File Name : roman_numerals.cpp
  * Creation Date : 02-23-2014
- * Last Modified : Sun 02 Mar 2014 10:18:59 PM AKST
+ * Last Modified : Sun 02 Mar 2014 10:27:13 PM AKST
  */
 
 #define CATCH_CONFIG_MAIN
@@ -15,4 +15,5 @@ char numeral(int a)
 TEST_CASE( "Roman numeral computation", "[numeral]" ) 
 {
     REQUIRE( numeral(1) == 'I' );
+    REQUIRE( numeral(1) == "II" );
 }
