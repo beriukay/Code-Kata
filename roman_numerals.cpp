43a50e05c07caa5cf4171756ef535a818bd2aaf2
paul <beriukay@gmail.com>
2014-03-02 22:34:16 -0900

    Change to std::string

--- roman_numerals.cpp
+++ roman_numerals.cpp
@@ -1,19 +1,20 @@
 /* Paul Gentemann
  * File Name : roman_numerals.cpp
  * Creation Date : 02-23-2014
- * Last Modified : Sun 02 Mar 2014 10:27:13 PM AKST
+ * Last Modified : Sun 02 Mar 2014 10:31:38 PM AKST
  */
 
 #define CATCH_CONFIG_MAIN
+#include <string>
 #include "catch.hpp"
 
-char numeral(int a)
+std::string numeral(int a)
 {   
-    return 'I'; 
+    return "I"; 
 }
 
 TEST_CASE( "Roman numeral computation", "[numeral]" ) 
 {
-    REQUIRE( numeral(1) == 'I' );
+    REQUIRE( numeral(1) == "I" );
     REQUIRE( numeral(1) == "II" );
 }
