68a1114b828ea32a36665a0c3d186a400d1baac9
paul <beriukay@gmail.com>
2014-03-03 22:44:29 -0900

    Add test for 32 that passes

--- roman_numerals.cpp
+++ roman_numerals.cpp
@@ -1,7 +1,7 @@
 /* Paul Gentemann
  * File Name : roman_numerals.cpp
  * Creation Date : 02-23-2014
- * Last Modified : Mon 03 Mar 2014 10:43:16 PM AKST
+ * Last Modified : Mon 03 Mar 2014 10:43:42 PM AKST
  */
 
 #define CATCH_CONFIG_MAIN
@@ -77,6 +77,7 @@ TEST_CASE( "Roman numeral computation", "[numeral]" )
 
     REQUIRE( numeral(30) == "XXX" );
     REQUIRE( numeral(31) == "XXXI" );
+    REQUIRE( numeral(32) == "XXXII" );
     REQUIRE( numeral(34) == "XXXIV" );
     REQUIRE( numeral(38) == "XXXVIII" );
     REQUIRE( numeral(50) == "L" );
