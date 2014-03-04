98a62fb8304bdac9eb30f4c0204c87866d202c52
paul <beriukay@gmail.com>
2014-03-03 21:32:04 -0900

    Added test for 4. Oh noesg++ roman_numerals.cpp -std=c++11 && ./a.out  It failedg++ roman_numerals.cpp -std=c++11 && ./a.out

--- roman_numerals.cpp
+++ roman_numerals.cpp
@@ -1,7 +1,7 @@
 /* Paul Gentemann
  * File Name : roman_numerals.cpp
  * Creation Date : 02-23-2014
- * Last Modified : Mon 03 Mar 2014 09:29:09 PM AKST
+ * Last Modified : Mon 03 Mar 2014 09:31:33 PM AKST
  */
 
 #define CATCH_CONFIG_MAIN
@@ -31,6 +31,7 @@ TEST_CASE( "Roman numeral computation", "[numeral]" )
     REQUIRE( numeral(1) == "I" );
     REQUIRE( numeral(2) == "II" );
     REQUIRE( numeral(3) == "III" );
+    REQUIRE( numeral(4) == "IV" );
     REQUIRE( numeral(5) == "V" );
     REQUIRE( numeral(6) == "VI" );
     REQUIRE( numeral(7) == "VII" );
