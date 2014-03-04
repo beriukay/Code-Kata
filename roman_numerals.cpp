e0deb090cf883358d8a707214acf12f958b0e6fe
paul <beriukay@gmail.com>
2014-03-03 22:06:32 -0900

    Reverted to before testing for 9

--- roman_numerals.cpp
+++ roman_numerals.cpp
@@ -1,7 +1,7 @@
 /* Paul Gentemann
  * File Name : roman_numerals.cpp
  * Creation Date : 02-23-2014
- * Last Modified : Mon 03 Mar 2014 09:48:46 PM AKST
+ * Last Modified : Mon 03 Mar 2014 10:06:02 PM AKST
  */
 
 #define CATCH_CONFIG_MAIN
@@ -17,11 +17,6 @@ string numeral(int num)
     string roman_num="";
     for(int value=0; value < critical_values.size(); ++value)
     {
-	if (num == 9)
-	{
-	    roman_num+="I";
-	    num+=1;
-	}
 	if (num == 4)
 	{
 	    roman_num+="I";
@@ -46,7 +41,6 @@ TEST_CASE( "Roman numeral computation", "[numeral]" )
     REQUIRE( numeral(6) == "VI" );
     REQUIRE( numeral(7) == "VII" );
     REQUIRE( numeral(8) == "VIII" );
-    REQUIRE( numeral(9) == "IX" );
     REQUIRE( numeral(10) == "X" );
     REQUIRE( numeral(11) == "XI" );
     REQUIRE( numeral(12) == "XII" );
