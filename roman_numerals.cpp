73544988465b4af9f004bf1fb120228402f75404
paul <beriukay@gmail.com>
2014-03-03 17:15:59 -0900

    Added test for 15, which passes without extra work

--- roman_numerals.cpp
+++ roman_numerals.cpp
@@ -1,7 +1,7 @@
 /* Paul Gentemann
  * File Name : roman_numerals.cpp
  * Creation Date : 02-23-2014
- * Last Modified : Mon 03 Mar 2014 05:14:12 PM AKST
+ * Last Modified : Mon 03 Mar 2014 05:15:47 PM AKST
  */
 
 #define CATCH_CONFIG_MAIN
@@ -39,4 +39,5 @@ TEST_CASE( "Roman numeral computation", "[numeral]" )
     REQUIRE( numeral(11) == "XI" );
     REQUIRE( numeral(12) == "XII" );
     REQUIRE( numeral(13) == "XIII" );
+    REQUIRE( numeral(15) == "XV" );
 }
