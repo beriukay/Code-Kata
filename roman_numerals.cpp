3130dba59db0db34d49e87a107a6cc0fcb8e874d
paul <beriukay@gmail.com>
2014-03-03 21:45:24 -0900

    Pass for 4

--- roman_numerals.cpp
+++ roman_numerals.cpp
@@ -1,7 +1,7 @@
 /* Paul Gentemann
  * File Name : roman_numerals.cpp
  * Creation Date : 02-23-2014
- * Last Modified : Mon 03 Mar 2014 09:31:33 PM AKST
+ * Last Modified : Mon 03 Mar 2014 09:42:00 PM AKST
  */
 
 #define CATCH_CONFIG_MAIN
@@ -17,6 +17,11 @@ string numeral(int num)
     string roman_num="";
     for(int value=0; value < critical_values.size(); ++value)
     {
+	if (num == 4)
+	{
+	    roman_num+="I";
+	    num+=1;
+	}
 	while(num>=critical_values[value])
 	{
 	    roman_num+=roman_values[value];
