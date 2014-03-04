614922b16eccbc83ffa8074afa66a59afa9e6700
paul <beriukay@gmail.com>
2014-03-03 17:26:05 -0900

    Added test for 58, which passes without extra work

--- roman_numerals.cpp
+++ roman_numerals.cpp
@@ -1,7 +1,7 @@
 /* Paul Gentemann
  * File Name : roman_numerals.cpp
  * Creation Date : 02-23-2014
- * Last Modified : Mon 03 Mar 2014 05:25:07 PM AKST
+ * Last Modified : Mon 03 Mar 2014 05:25:47 PM AKST
  */
 
 #define CATCH_CONFIG_MAIN
@@ -49,4 +49,5 @@ TEST_CASE( "Roman numeral computation", "[numeral]" )
     REQUIRE( numeral(38) == "XXXVIII" );
     REQUIRE( numeral(50) == "L" );
     REQUIRE( numeral(55) == "LV" );
+    REQUIRE( numeral(58) == "LVIII" );
 }
