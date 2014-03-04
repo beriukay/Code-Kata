84c0f5f6b650d1f95be29117652f6b8eb9b2b06f
paul <beriukay@gmail.com>
2014-03-03 17:27:57 -0900

    Added test for 70, which passes without extra work

--- roman_numerals.cpp
+++ roman_numerals.cpp
@@ -1,7 +1,7 @@
 /* Paul Gentemann
  * File Name : roman_numerals.cpp
  * Creation Date : 02-23-2014
- * Last Modified : Mon 03 Mar 2014 05:27:12 PM AKST
+ * Last Modified : Mon 03 Mar 2014 05:27:40 PM AKST
  */
 
 #define CATCH_CONFIG_MAIN
@@ -52,4 +52,5 @@ TEST_CASE( "Roman numeral computation", "[numeral]" )
     REQUIRE( numeral(58) == "LVIII" );
     REQUIRE( numeral(60) == "LX" );
     REQUIRE( numeral(68) == "LXVIII" );
+    REQUIRE( numeral(70) == "LXX" );
 }
