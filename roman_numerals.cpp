acc71814c27af0d93d474edf4d80706d3053a4f1
paul <beriukay@gmail.com>
2014-03-03 23:41:18 -0900

    Add test for 90

--- roman_numerals.cpp
+++ roman_numerals.cpp
@@ -1,7 +1,7 @@
 /* Paul Gentemann
  * File Name : roman_numerals.cpp
  * Creation Date : 02-23-2014
- * Last Modified : Mon 03 Mar 2014 11:39:51 PM AKST
+ * Last Modified : Mon 03 Mar 2014 11:40:57 PM AKST
  */
 
 #define CATCH_CONFIG_MAIN
@@ -122,6 +122,8 @@ TEST_CASE( "Roman numeral computation", "[numeral]" )
     REQUIRE( numeral(78) == "LXXVIII" );
 
     REQUIRE( numeral(88) == "LXXXVIII" );
+
+    REQUIRE( numeral(90) == "XC" );
 
     REQUIRE( numeral(100) == "C" );
     REQUIRE( numeral(333) == "CCCXXXIII" );
